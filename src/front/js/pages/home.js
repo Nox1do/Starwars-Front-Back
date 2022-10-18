1c6fe4cf95403f25c69192da86686417ee678a15
Nox1do <noogux@gmail.com>
2022-10-18 05:31:53 +0000

    fixed error with front end

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,30 +6,31 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const token = (sessionStorage.setItem("token");
+  const token = sessionStorage.getItem("token");
+
+  console.log("this is your token", token)
 
   const handleClick = () => {
     const postFetch = {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: email,
         password: password,
       }),
     };
-    fetch(
-      "https://3001-4geeksacade-reactflaskh-ts1xu7izylx.ws-us71.gitpod.io/api/token", postFetch)
-      .then(resp => {
+    fetch("https://3001-4geeksacade-reactflaskh-ts1xu7izylx.ws-us71.gitpod.io/api/token",postFetch)
+      .then((resp) => {
         if (resp.status === 200) return resp.json();
         else alert("There has been some error");
       })
-      .then(data => {
+      .then((data) => {
         console.log("this came from the backend", data);
         sessionStorage.setItem("token", data.access_token);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("You have a error", error);
       });
   };
@@ -37,25 +38,25 @@ export const Home = () => {
   return (
     <div className="text-center mt-5">
       <h1>Hello Rigo!!</h1>
-      
+    
         {(token && token != "" && token != undefined)  ? "You are logged in with this token" + token:
 	<div>
-		 <input
-		 type="text"
-		 placeholder="email"
-		 value={email}
-		 onChange={(e) => setEmail(e.target.value)}
-	   />
-	   <input
-		 type="password"
-		 placeholder="password"
-		 value={password}
-		 onChange={(e) => setPassword(e.target.value)}
-	   />
-	   <button onClick={handleClick}>Login</button>
-	 </div>
-   
+       <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleClick}>Login</button>
+    </div>
 		};
-    </div> 
+       
+    </div>
   );
 };
