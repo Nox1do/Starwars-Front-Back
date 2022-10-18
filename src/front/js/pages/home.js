9998cc3f7ed32582209d01ab707551075df653f1
Nox1do <noogux@gmail.com>
2022-10-18 04:58:52 +0000

    first commit with frond-end error

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,61 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const token = (sessionStorage.setItem("token");
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const handleClick = () => {
+    const postFetch = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    };
+    fetch(
+      "https://3001-4geeksacade-reactflaskh-ts1xu7izylx.ws-us71.gitpod.io/api/token", postFetch)
+      .then(resp => {
+        if (resp.status === 200) return resp.json();
+        else alert("There has been some error");
+      })
+      .then(data => {
+        console.log("this came from the backend", data);
+        sessionStorage.setItem("token", data.access_token);
+      })
+      .catch(error => {
+        console.log("You have a error", error);
+      });
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <h1>Hello Rigo!!</h1>
+      
+        {(token && token != "" && token != undefined)  ? "You are logged in with this token" + token:
+	<div>
+		 <input
+		 type="text"
+		 placeholder="email"
+		 value={email}
+		 onChange={(e) => setEmail(e.target.value)}
+	   />
+	   <input
+		 type="password"
+		 placeholder="password"
+		 value={password}
+		 onChange={(e) => setPassword(e.target.value)}
+	   />
+	   <button onClick={handleClick}>Login</button>
+	 </div>
+   
+		};
+    </div> 
+  );
 };
